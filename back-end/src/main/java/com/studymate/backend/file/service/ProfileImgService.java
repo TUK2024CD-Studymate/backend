c781db47708df4f07aec25233445b90494009723
정우혁 <alalalalalalall367@gmail.com>
2024-01-23 00:08:41 +0900

    feat: ProfileImgService에 이미지를 DB에 저장하는 로직 구현

--- back-end/src/main/java/com/studymate/backend/file/service/ProfileImgService.java
+++ back-end/src/main/java/com/studymate/backend/file/service/ProfileImgService.java
@@ -1,81 +1,44 @@
 package com.studymate.backend.file.service;
 
+import com.studymate.backend.config.security.SecurityUtil;
 import com.studymate.backend.file.ProfileImgRepository;
 import com.studymate.backend.file.domain.ProfileImg;
-import com.studymate.backend.file.dto.ProfileImgResponse;
-import com.studymate.backend.file.dto.ProfileImgUpload;
 import com.studymate.backend.member.MemberRepository;
 import com.studymate.backend.member.domain.Member;
+import com.studymate.backend.member.exception.NotFoundMemberException;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProfileImgService {
-
-    private static final Logger logger = LoggerFactory.getLogger(ProfileImgService.class);
-
     private final MemberRepository memberRepository;
     private final ProfileImgRepository profileImgRepository;
 
-    @Value("${file.profileImagePath}")
-    private String uploadFolder;
+    @Transactional
+    public String upload(MultipartFile upload) throws IOException {
 
-    public void upload(ProfileImgUpload upload, String email) {
-        Member member = memberRepository.findOneWithAuthoritiesByEmail(email).orElseThrow(() ->
-                new UsernameNotFoundException("조회 실패"));
-        MultipartFile file = upload.getFile();
+        Member member = SecurityUtil.getCurrentUsername()
+                .flatMap(memberRepository::findOneWithAuthoritiesByEmail)
+                .orElseThrow(() -> new NotFoundMemberException("Member not found"));
 
-        UUID uuid = UUID.randomUUID();
-        String imageFileName = uuid + "_" + file.getOriginalFilename();
-
-        File destinationFile = new File(uploadFolder + imageFileName);
-
-        try {
-            file.transferTo(destinationFile);
-
-            ProfileImg image = profileImgRepository.findByMember(member);
-
-            if (image != null) {
-                image.updateUrl("/profileImages/" + imageFileName);
-            } else {
-                image = ProfileImg.builder()
-                        .member(member)
-                        .url("/profileImages/" + imageFileName)
-                        .build();
-
-            }
-            profileImgRepository.save(image);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public ProfileImgResponse findImage(String email) {
-        Member member = memberRepository.findOneWithAuthoritiesByEmail(email).orElseThrow(() ->
-                new RuntimeException("조회 실패"));
         ProfileImg image = profileImgRepository.findByMember(member);
 
-        String defaultImageUrl = "/profileImages/anonymous.png";
+        image.upload(upload.getOriginalFilename(), upload.getContentType(), upload.getBytes());
 
-        if (image == null) {
-            return ProfileImgResponse.builder()
-                    .url(defaultImageUrl)
-                    .build();
-        } else {
-            return ProfileImgResponse.builder()
-                    .url(image.getUrl())
-                    .build();
+        if (image != null) {
+            log.info("imageDataName: {}", image.getName());
+            return "업로드 성공 : " + upload.getOriginalFilename();
         }
+
+        return null;
     }
 }
+
