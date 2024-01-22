042570b26d6d4ee628858a00b65ea32a764c3668
정우혁 <alalalalalalall367@gmail.com>
2024-01-23 00:05:22 +0900

    feat: 이미지 엔티티에 이미지 데이터 저장하기 위한 필드(imageData) 추가

--- back-end/src/main/java/com/studymate/backend/file/domain/ProfileImg.java
+++ back-end/src/main/java/com/studymate/backend/file/domain/ProfileImg.java
@@ -6,8 +6,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Builder
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfileImg extends BaseTimeEntity {
@@ -16,8 +16,13 @@ public class ProfileImg extends BaseTimeEntity {
     @Column(name = "img_id")
     private Long id;
 
-    @Column(nullable = false)
-    private String url;
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Column(name = "imagedata", length = 1000)
+    private byte[] imageData;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
