57410a6fe280cf4a232b2728bd920300aae56bf1
정우혁 <alalalalalalall367@gmail.com>
2024-01-23 23:25:12 +0900

    feat: MemberLoginRequest에 swagger 명세 및 예시 데이터 추가

--- back-end/src/main/java/com/studymate/backend/member/dto/MemberLoginRequest.java
+++ back-end/src/main/java/com/studymate/backend/member/dto/MemberLoginRequest.java
@@ -1,5 +1,6 @@
 package com.studymate.backend.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
@@ -9,10 +10,13 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Memeber login reqeust")
 public class MemberLoginRequest {
     @NotBlank
     @Email
+    @Schema(description = "사용자 이메일", nullable = false, example = "mr6208@naver.com")
     private String email;
     @NotBlank
+    @Schema(description = "사용자 비밀번호", nullable = false, example = "asdf1020")
     private String password;
 }
