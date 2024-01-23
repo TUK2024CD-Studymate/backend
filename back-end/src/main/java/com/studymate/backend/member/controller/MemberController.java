06baf99df4e6794dae4bb47cf242fed3355730e0
정우혁 <alalalalalalall367@gmail.com>
2024-01-23 23:21:26 +0900

    feat: 회원가입 url 수정 및 모든 요청에 swagger 명세 추가

--- back-end/src/main/java/com/studymate/backend/member/controller/MemberController.java
+++ back-end/src/main/java/com/studymate/backend/member/controller/MemberController.java
@@ -4,7 +4,10 @@ import com.studymate.backend.config.security.jwt.JwtFilter;
 import com.studymate.backend.config.security.jwt.TokenProvider;
 import com.studymate.backend.member.dto.*;
 import com.studymate.backend.member.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -20,18 +23,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Member", description = "Member API")
 public class MemberController {
 
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
     private final MemberService memberService;
-    @PostMapping("/sign-in")
-    public ResponseEntity<MemberResponse> signIn(@Valid @RequestBody MemberRequest request) {
+
+    @PostMapping("/signIn")
+    @Operation(summary = "member sign in", description = "회원이 회원가입을 한다.")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
+    public ResponseEntity<?> signIn(@Valid @RequestBody MemberRequest request) {
         return ResponseEntity.ok(memberService.signup(request));
     }
 
     @PostMapping("/login")
+    @Operation(summary = "member login", description = "회원이 로그인을 한다.")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
     public ResponseEntity<TokenDto> authorize(@Valid @RequestBody MemberLoginRequest request) {
 
         UsernamePasswordAuthenticationToken authenticationToken =
@@ -50,18 +58,24 @@ public class MemberController {
 
     @GetMapping("/user")
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @Operation(summary = "get member info", description = "회원정보를 단건으로 조회한다")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
     public ResponseEntity<MemberResponse> getMyUserInfo() {
         return ResponseEntity.ok(memberService.getMyMemberWithAuthorities());
     }
 
     @PutMapping("/user")
     @PreAuthorize("hasAnyRole('USER')")
+    @Operation(summary = "member update", description = "프로필 이미지를 제외한 회원정보를 수정한다")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
     public ResponseEntity<MemberResponse> update(@Valid @RequestBody MemberUpdateRequest request) {
         return ResponseEntity.ok(memberService.update(request));
     }
 
     @DeleteMapping("/user")
     @PreAuthorize("hasAnyRole('USER')")
+    @Operation(summary = "member soft delete", description = "회원탈퇴(논리삭제)를 한다.")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
     public ResponseEntity<?> delete() {
         return ResponseEntity.ok(memberService.delete());
     }
