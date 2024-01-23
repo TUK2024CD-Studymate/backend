db2aa89709404fa2169af0e613ad73cc156b3f4b
jihye9871 <jihye9871@naver.com>
2024-01-24 04:04:29 +0900

    feat: Post Entity 구현

---
+++ back-end/src/main/java/com/studymate/backend/post/domain/Post.java
@@ -0,0 +1,36 @@
+package com.studymate.backend.post.domain;
+
+import com.studymate.backend.global.BaseTimeEntity;
+import com.studymate.backend.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false, updatable = false)
+    private Member member;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @Column(nullable = false)
+    private String category;
+
+    private Boolean isDeleted;
+
+}
