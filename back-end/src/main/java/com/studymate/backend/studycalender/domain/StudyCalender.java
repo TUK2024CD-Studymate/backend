fd9d105c11e9b66d390f7b157be378869020ac1b
정우혁 <alalalalalalall367@gmail.com>
2024-01-25 21:54:23 +0900

    feat: StudyCalender entity 설계

---
+++ back-end/src/main/java/com/studymate/backend/studycalender/domain/StudyCalender.java
@@ -0,0 +1,39 @@
+package com.studymate.backend.studycalender.domain;
+
+import com.studymate.backend.global.BaseTimeEntity;
+import com.studymate.backend.member.domain.Interests;
+import com.studymate.backend.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@Table(name = "study_calender")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyCalender extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "calender_id")
+    private Long id;
+
+    private String content;
+
+    @Enumerated(value = EnumType.STRING)
+    private Interests studyClass;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+}
