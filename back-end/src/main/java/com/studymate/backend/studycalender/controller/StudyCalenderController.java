fc777cd99d86643f9e117a9ac220ae1370a5f9e9
정우혁 <alalalalalalall367@gmail.com>
2024-01-26 17:39:36 +0900

    feat: StudyCalenderController에 단일조회 api 구현

--- back-end/src/main/java/com/studymate/backend/studycalender/controller/StudyCalenderController.java
+++ back-end/src/main/java/com/studymate/backend/studycalender/controller/StudyCalenderController.java
@@ -1,6 +1,7 @@
 package com.studymate.backend.studycalender.controller;
 
 import com.studymate.backend.studycalender.dto.CalenderCreateRequest;
+import com.studymate.backend.studycalender.dto.CalenderResponse;
 import com.studymate.backend.studycalender.service.StudyCalenderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -8,10 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +19,16 @@ public class StudyCalenderController {
     private final StudyCalenderService studyCalenderService;
 
     @PostMapping("/calender")
-    @Operation(summary = "create post", description = "회원이 스터디 기록을 생성한다.")
+    @Operation(summary = "create calender", description = "회원이 스터디 기록을 생성한다.")
     @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
     public ResponseEntity<String> createPost(@Valid @RequestBody CalenderCreateRequest request) {
         return ResponseEntity.ok(studyCalenderService.createPost(request));
+    }
+
+    @GetMapping("/calender/{calender_id}")
+    @Operation(summary = "get calender info", description = "회원이 작성했던 스터디 기록을 조회한다.")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
+    public ResponseEntity<CalenderResponse> findCalender(@PathVariable("calender_id") Long id) {
+        return ResponseEntity.ok(studyCalenderService.findOne(id));
     }
 }
