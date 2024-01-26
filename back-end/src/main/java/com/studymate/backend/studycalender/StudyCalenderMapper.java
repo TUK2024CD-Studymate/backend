d84268a1b90f61056ed83d3ad886d322b7c86b1d
정우혁 <alalalalalalall367@gmail.com>
2024-01-26 17:40:46 +0900

    feat: Mapper클래스에 Response로 매핑해주는 로직 추가

--- back-end/src/main/java/com/studymate/backend/studycalender/StudyCalenderMapper.java
+++ back-end/src/main/java/com/studymate/backend/studycalender/StudyCalenderMapper.java
@@ -3,6 +3,7 @@ package com.studymate.backend.studycalender;
 import com.studymate.backend.member.domain.Member;
 import com.studymate.backend.studycalender.domain.StudyCalender;
 import com.studymate.backend.studycalender.dto.CalenderCreateRequest;
+import com.studymate.backend.studycalender.dto.CalenderResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,18 @@ public class StudyCalenderMapper {
                 .startTime(request.getStartTime())
                 .member(member)
                 .build();
+        return calender;
+    }
+
+    public CalenderResponse toResponse(StudyCalender studyCalender) {
+
+        CalenderResponse calender = CalenderResponse.builder()
+                .content(studyCalender.getContent())
+                .endTime(studyCalender.getEndTime())
+                .studyClass(studyCalender.getStudyClass())
+                .startTime(studyCalender.getStartTime())
+                .build();
+
         return calender;
     }
 }
