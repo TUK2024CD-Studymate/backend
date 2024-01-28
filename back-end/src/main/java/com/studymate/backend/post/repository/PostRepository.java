622fca71d661612fa28cf05eae86a933bd9a6747
jihye9871 <jihye9871@naver.com>
2024-01-28 15:49:02 +0900

    feat: PostController 수정

--- back-end/src/main/java/com/studymate/backend/post/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studymate.backend.post.repository;
-
-import com.studymate.backend.post.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
