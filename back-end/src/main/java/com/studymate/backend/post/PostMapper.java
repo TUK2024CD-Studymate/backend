c1bb086ca1077bf0d7203b69094b6dfe49ea694d
jihye9871 <jihye9871@naver.com>
2024-02-05 16:38:14 +0900

    feat: Post Mapper category 변경, interests, 모집여부 추가

--- back-end/src/main/java/com/studymate/backend/post/PostMapper.java
+++ back-end/src/main/java/com/studymate/backend/post/PostMapper.java
@@ -1,5 +1,6 @@
 package com.studymate.backend.post;
 
+import com.studymate.backend.member.domain.Category;
 import com.studymate.backend.member.domain.Member;
 import com.studymate.backend.post.domain.Post;
 import com.studymate.backend.post.dto.PostRequestDto;
@@ -15,6 +16,9 @@ public class PostMapper {
                 .title(request.getTitle())
                 .content(request.getContent())
                 .category(request.getCategory())
+                .interests(request.getInterests())
+                // 카테고리가 STUDY인 경우에만 recruitmentStatus 설정
+                .recruitmentStatus(request.getCategory() == Category.STUDY ? request.getRecruitmentStatus() : null)
                 .build();
     }
 
@@ -24,13 +28,14 @@ public class PostMapper {
         Member member = post.getMember();
         String nickname = (member != null) ? member.getNickname() : null;
 
-
         return PostResponseDto.builder()
                 .id(post.getId())
                 .title(post.getTitle())
                 .content(post.getContent())
                 .category(post.getCategory())
                 .createdAt(post.getCreatedAt().toString()) // 날짜 포맷은 필요에 따라 변경
+                .interests(post.getInterests())
+                .recruitmentStatus(post.getRecruitmentStatus()) // 모집 여부 추가
                 .nickname(nickname)
                 .build();
     }
