8c634cdf47ad3887a4f12dd9048e522f6a213705
jihye9871 <jihye9871@naver.com>
2024-02-05 16:39:42 +0900

    feat: PostUpdateRequestDto category 변경, interests, 모집여부 추가

--- back-end/src/main/java/com/studymate/backend/post/dto/PostUpdateRequestDto.java
+++ back-end/src/main/java/com/studymate/backend/post/dto/PostUpdateRequestDto.java
@@ -1,8 +1,11 @@
 package com.studymate.backend.post.dto;
 
 
+import com.studymate.backend.member.domain.Category;
+import com.studymate.backend.member.domain.Interests;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,11 +22,18 @@ public class PostUpdateRequestDto {
     private String title;
 
     @NotBlank
-    @Schema(description = "사용자가 바ㄹ꿀 게시글 내용", nullable = false, example = "스프링부트는 어렵다^^")
+    @Schema(description = "사용자가 바꿀 게시글 내용", nullable = false, example = "스프링부트는 어렵다^^")
     private String content;
 
-    @NotBlank
-    @Schema(description = "사용자가 바꿀 게시글 카테고리", nullable = false, example = "코딩")
-    private String category;
+    @NotNull
+    @Schema(description = "사용자가 바꿀 게시글 카테고리", nullable = false, example = "STUDY")
+    private Category category;
+
+    @NotNull
+    @Schema(description = "사용자가 바꿀 관심 분야", nullable = false)
+    private Interests interests;
+
+    @Schema(description = "스터디 모집 여부 변경 (스터디 카테고리인 경우만 적용)", nullable = true)
+    private Boolean recruitmentStatus;
 
 }
