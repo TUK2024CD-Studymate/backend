c52214a4dad646b8b807117b85781982e7969ade
jihye9871 <jihye9871@naver.com>
2024-02-05 16:38:34 +0900

    feat: PostRequestDto category 변경, interests, 모집여부 추가

--- back-end/src/main/java/com/studymate/backend/post/dto/PostRequestDto.java
+++ back-end/src/main/java/com/studymate/backend/post/dto/PostRequestDto.java
@@ -1,5 +1,7 @@
 package com.studymate.backend.post.dto;
 
+import com.studymate.backend.member.domain.Category;
+import com.studymate.backend.member.domain.Interests;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,11 +25,18 @@ public class PostRequestDto {
     @Schema(description = "게시글 내용", nullable = false, example = "스프링부트는 어렵다^^")
     private String content;
 
-    @NotBlank
-    @Schema(description = "게시글 카테고리", nullable = false, example = "코딩")
-    private String category;
+    @NotNull
+    @Schema(description = "게시글 카테고리", nullable = false, example = "STUDY")
+    private Category category;
 
     @NotNull
-    @Schema(description = "회원 ID", nullable = false, example = "정환코딩")
+    @Schema(description = "관심 분야", nullable = false, example = "PROGRAMMING")
+    private Interests interests;
+
+    @Schema(description = "스터디 모집 여부 (스터디 카테고리인 경우만 적용)", nullable = true, example = "true")
+    private Boolean recruitmentStatus;
+
+    @NotNull
+    @Schema(description = "회원 ID", nullable = false, example = "1")
     private Long user_id;
 }
