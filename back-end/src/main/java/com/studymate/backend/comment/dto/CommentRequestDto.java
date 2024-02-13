a43a7e76f6288173c69bc0702658c8a61ef443bc
jihye9871 <jihye9871@naver.com>
2024-02-14 00:16:22 +0900

    feat: CommentRequestDto에서 post_id 삭제

--- back-end/src/main/java/com/studymate/backend/comment/dto/CommentRequestDto.java
+++ back-end/src/main/java/com/studymate/backend/comment/dto/CommentRequestDto.java
@@ -13,12 +13,9 @@ import lombok.NoArgsConstructor;
 @Builder
 @Schema(description = "Comment create request")
 public class CommentRequestDto {
+
     @NotBlank
     @Schema(description = "댓글 내용", nullable = false, example = "안녕하시렵니까")
     private String content;
-
-    @Schema(description = "댓글을 남길 게시글", nullable = false, example = "1")
-    private Long post_id;
-
 
 }
