a8c5369129841003646c6f87dbc0926dd4102994
jihye9871 <jihye9871@naver.com>
2024-02-17 17:14:52 +0900

    feat: ChatServiceValidator 구현

---
+++ back-end/src/main/java/com/studymate/backend/chat/service/ChatServiceValidator.java
@@ -0,0 +1,36 @@
+package com.studymate.backend.chat.service;
+
+import com.studymate.backend.chat.entity.ChatParticipation;
+import com.studymate.backend.chat.entity.ChattingRoom;
+import com.studymate.backend.chat.error.ChatParticipationNotFoundException;
+import com.studymate.backend.chat.repository.ChatParticipationRepository;
+import com.studymate.backend.chat.repository.ChattingRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ChatServiceValidator {
+    private final ChatParticipationRepository chatParticipationRepository;
+    private final ChattingRoomRepository chattingRoomRepository;
+
+    public ChattingRoom validateChattingRoomExists(Long chattingRoomId) {
+        return chattingRoomRepository.findById(chattingRoomId)
+                .orElseThrow(() -> new IllegalArgumentException("채팅룸 아이디에 해당하는 채팅룸이 존재하지 않습니다: " + chattingRoomId));
+    }
+
+    public List<ChatParticipation> validateUserParticipation(Long userId) {
+        List<ChatParticipation> participations = chatParticipationRepository.findByUserId(userId);
+        if (participations.isEmpty()) {
+            throw new ChatParticipationNotFoundException("사용자 ID에 해당하는 채팅 참여내역을 찾을 수 없습니다: " + userId);
+        }
+        return participations;
+    }
+
+    public boolean checkForDuplicatedParticipation(Long userId) {
+        return chatParticipationRepository.existsByUserId(userId);
+    }
+
+}
