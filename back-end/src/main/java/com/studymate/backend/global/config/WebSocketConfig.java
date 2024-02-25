2910646c5fb2f4f581269d4c9bc013ba6f8c8d19
jihye9871 <jihye9871@naver.com>
2024-02-26 01:29:26 +0900

    feat: WebsocketConfig 구현 중

---
+++ back-end/src/main/java/com/studymate/backend/global/config/WebSocketConfig.java
@@ -0,0 +1,6 @@
+package com.studymate.backend.global.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
