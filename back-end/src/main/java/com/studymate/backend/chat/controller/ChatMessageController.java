24c60e852f59de9215dc62a378ec6dde708b817d
jihye9871 <jihye9871@naver.com>
2024-02-26 01:24:02 +0900

    feat: ChatMessageController 구현

--- back-end/src/main/java/com/studymate/backend/chat/controller/ChatMessageController.java
+++ back-end/src/main/java/com/studymate/backend/chat/controller/ChatMessageController.java
@@ -1,20 +1,33 @@
 package com.studymate.backend.chat.controller;
 
+import com.studymate.backend.chat.dto.request.CreateMessageRequest;
 import com.studymate.backend.chat.dto.response.ChatMessageResponse;
 import com.studymate.backend.chat.service.ChattingRoomService;
+import com.studymate.backend.member.domain.Member;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
-@RequestMapping("/chats")
+@RequestMapping("/chat")
 public class ChatMessageController {
     private final ChattingRoomService chattingRoomService;
 
+    public void chat(StompHeaderAccessor headerAccessor,
+                     @DestinationVariable Long chattingRoomId,
+                     @Payload CreateMessageRequest messageRequest){
+
+        Member member = (Member) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("member");
+
+    }
     public ChatMessageController(ChattingRoomService chattingRoomService) {
         this.chattingRoomService = chattingRoomService;
     }
---
+++ back-end/src/main/java/com/studymate/backend/global/RabbitMQ.java
@@ -0,0 +1,7 @@
+package com.studymate.backend.global;
+
+public class RabbitMQ {
+    public static final String CHAT_QUEUE_NAME = "chat.queue";
+    public static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+    public static final String ROUTING_KEY = "room.*";
+}
