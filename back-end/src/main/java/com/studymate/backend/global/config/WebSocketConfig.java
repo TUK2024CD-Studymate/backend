884058621894508b846f90df7fa3bd4bb2ec6f2d
jihye9871 <jihye9871@naver.com>
2024-03-06 02:30:59 +0900

    feat: config 추가

--- back-end/src/main/java/com/studymate/backend/global/config/WebSocketConfig.java
+++ back-end/src/main/java/com/studymate/backend/global/config/WebSocketConfig.java
@@ -1,6 +1,49 @@
 package com.studymate.backend.global.config;
 
+import com.studymate.backend.global.stomp.StompExceptionHandler;
+import com.studymate.backend.global.stomp.StompHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompHandler stompHandler;
+    private final StompExceptionHandler stompExceptionHandler;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+                .setRelayHost("rabbitmq")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/pub");
+//        registry.setApplicationDestinationPrefixes("/pub");
+        registry.setPathMatcher(new AntPathMatcher("."));
+    }
+    @Override
+    public void registerStompEndpoints (StompEndpointRegistry registry){
+        registry.setErrorHandler(stompExceptionHandler.getErrorHandler())
+                .addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+
 }
