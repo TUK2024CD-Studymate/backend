b6dde92d18a14da8467f8c27bbb4403f74e00370
jihye9871 <jihye9871@naver.com>
2024-03-13 23:19:42 +0900

    feat: ChatRoom jwt에 맞춰서 수정

--- back-end/src/main/java/com/studymate/backend/chat/domain/ChatRoom.java
+++ back-end/src/main/java/com/studymate/backend/chat/domain/ChatRoom.java
@@ -1,23 +1,27 @@
 package com.studymate.backend.chat.domain;
 
+import com.studymate.backend.global.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@Setter
-@NoArgsConstructor
-public class ChatRoom {
+@Builder
+@Entity
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom extends BaseTimeEntity {
 
-    private String roomId;
-    private String roomName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_room_id")
+    private Long id;
 
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private final List<ChatMessage> chatMessageList = new ArrayList<>();
 
-    public static ChatRoom create(String name) {
-        ChatRoom room = new ChatRoom();
-        room.roomId = UUID.randomUUID().toString();
-        room.roomName = name;
-        return room;
-    }
+    @OneToMany(mappedBy = "chatRoom", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private final List<UserChatRoom> userChatRoomList = new ArrayList<>();
 }
