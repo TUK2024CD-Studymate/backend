696e1bd3efead1374b72f9a438f61b5782d6bee6
jihye9871 <jihye9871@naver.com>
2024-03-13 23:20:54 +0900

    feat: MessageController jwt에 맞춰서 수정

--- back-end/src/main/java/com/studymate/backend/chat/controller/MessageController.java
+++ back-end/src/main/java/com/studymate/backend/chat/controller/MessageController.java
@@ -1,22 +1,39 @@
 package com.studymate.backend.chat.controller;
 
 import com.studymate.backend.chat.domain.ChatMessage;
+import com.studymate.backend.chat.dto.CreateMessageReq;
+import com.studymate.backend.chat.service.ChatService;
+import com.studymate.backend.member.domain.UserDetail;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequiredArgsConstructor
+import java.util.Objects;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class MessageController {
+    private final ChatService chatService;
 
-    private final SimpMessageSendingOperations sendingOperations;
+    @MessageMapping("chat.message.{chatRoomId}")
+    public void chat(StompHeaderAccessor headerAccessor,
+                     @DestinationVariable Long chatRoomId,
+                     @Payload CreateMessageReq messageRequest) {
+        UserDetail userDetail = (UserDetail) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("userDetail");
 
-    @MessageMapping("/chat/message")
-    public void enter(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-            message.setMessage(message.getSender()+"님이 입장하였습니다.");
-        }
-        sendingOperations.convertAndSend("/topic/chat/room/"+message.getRoomId(),message);
+//        rabbitTemplate.convertAndSend(CHAT_EXCHANGE_NAME, "room." + chatRoomId, messageRequest);
+
+        chatService.saveMessage(userDetail.getMember(), chatRoomId, messageRequest);
+        log.info("Message [{}] send by user: {} to chatting room: {}", messageRequest.getContent(), messageRequest.getSender(), chatRoomId);
     }
+
+    // 메세지가 큐에 도착할 때 실행 - rabbitmq 연결
 }
