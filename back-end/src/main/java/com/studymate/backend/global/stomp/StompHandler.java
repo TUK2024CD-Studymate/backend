f1741de08b23269a6a21e34930f33e675d9e4ae2
jihye9871 <jihye9871@naver.com>
2024-03-13 23:29:12 +0900

    feat: StompHandler 구현

---
+++ back-end/src/main/java/com/studymate/backend/global/stomp/StompHandler.java
@@ -0,0 +1,28 @@
+package com.studymate.backend.global.stomp;
+
+import com.studymate.backend.config.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class StompHandler implements ChannelInterceptor {
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if( StompCommand.CONNECT == accessor.getCommand()) {
+            final String authorization = tokenProvider.extractJwt(accessor);
+
+            tokenProvider.validateToken(authorization);
+        }
+        return message;
+    }
+}
