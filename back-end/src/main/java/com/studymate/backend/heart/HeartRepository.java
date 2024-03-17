576657429361975cc97bdcadfaff1fece3ba6e3d
정우혁 <alalalalalalall367@gmail.com>
2024-03-17 22:23:41 +0900

    feat: HeartRepository 내부에 회원별로 누른 좋아요를 반환해주는 쿼리메소드 설계

--- back-end/src/main/java/com/studymate/backend/heart/HeartRepository.java
+++ back-end/src/main/java/com/studymate/backend/heart/HeartRepository.java
@@ -5,8 +5,11 @@ import com.studymate.backend.member.domain.Member;
 import com.studymate.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
     Optional<Heart> findByMemberAndPost(Member member, Post post);
+
+    List<Heart> findAllByMember(Member member);
 }
