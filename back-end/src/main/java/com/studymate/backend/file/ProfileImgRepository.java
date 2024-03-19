205bef6de684a0716d0d68de13e241e379200c04
jihye9871 <jihye9871@naver.com>
2024-03-19 14:20:38 +0900

    feat: ChatMessage Entity 구현

--- back-end/src/main/java/com/studymate/backend/file/ProfileImgRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studymate.backend.file;
-
-import com.studymate.backend.file.domain.ProfileImg;
-import com.studymate.backend.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileImgRepository extends JpaRepository<ProfileImg, Long> {
-//    ProfileImg findByMember(Member member);
-}
