4a8d9846179c7e431886289ed55a4722f82a5ce0
jihye9871 <jihye9871@naver.com>
2024-03-23 17:50:47 +0900

    feat: PostMapper에서 response 값에 댓글 수 추가

--- back-end/src/main/java/com/studymate/backend/post/PostMapper.java
+++ back-end/src/main/java/com/studymate/backend/post/PostMapper.java
@@ -1,14 +1,18 @@
 package com.studymate.backend.post;
 
+import com.studymate.backend.comment.service.CommentService;
 import com.studymate.backend.member.domain.Category;
 import com.studymate.backend.member.domain.Member;
 import com.studymate.backend.post.domain.Post;
 import com.studymate.backend.post.dto.PostRequestDto;
 import com.studymate.backend.post.dto.PostResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PostMapper {
+    private final CommentService commentService;
 
     public Post toEntity(PostRequestDto request, Member member) {
         return Post.builder()
@@ -26,6 +30,8 @@ public class PostMapper {
     public PostResponseDto toResponse(Post post) {
         if (post == null) return null;
 
+        // 댓글 수 조회
+        Long commentCount = commentService.countCommentsByPostId(post.getId());
         Member member = post.getMember();
         String nickname = (member != null) ? member.getNickname() : null;
 
@@ -39,6 +45,7 @@ public class PostMapper {
                 .interests(post.getInterests())
                 .recruitmentStatus(post.getRecruitmentStatus()) // 모집 여부 추가
                 .nickname(nickname)
+                .commentCount(commentCount) // 댓글 수 추가
                 .build();
     }
 
