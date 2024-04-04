c837d848d41f69f27a7796a21d20eca6d414c665
정우혁 <alalalalalalall367@gmail.com>
2024-04-04 14:54:25 +0900

    feat: SecurityConfig 내부 Redis 추가

--- back-end/src/main/java/com/studymate/backend/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/studymate/backend/config/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.studymate.backend.config.security.jwt.JwtSecurityConfig;
 import com.studymate.backend.config.security.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,15 +27,19 @@ public class SecurityConfig {
     private final CorsFilter corsFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final RedisTemplate<String,String> redisTemplate;
 
     public SecurityConfig(TokenProvider tokenProvider,
                           CorsFilter corsFilter,
                           JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler,
+                          RedisTemplate<String, String> redisTemplate) {
+
         this.tokenProvider = tokenProvider;
         this.corsFilter = corsFilter;
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.redisTemplate = redisTemplate;
     }
 
     @Bean
@@ -54,12 +59,13 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                                .requestMatchers("/api/signIn/**", "api/login/**").permitAll()
-                                .requestMatchers("/api/meeting/**").permitAll()
-                                .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                                .requestMatchers("/ws/**").permitAll()
-                                .requestMatchers("/api/chat/**").permitAll()
-                                .anyRequest().authenticated()
+                        .requestMatchers("/api/signIn/**", "api/login/**").permitAll()
+                        .requestMatchers("/api/meeting/**").permitAll()
+                        .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
+                        .requestMatchers("/api/chat/**").permitAll()
+                        .requestMatchers("/api/logout").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -68,7 +74,7 @@ public class SecurityConfig {
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                 )
 
-                .with(new JwtSecurityConfig(tokenProvider), customizer -> {
+                .with(new JwtSecurityConfig(tokenProvider, redisTemplate), customizer -> {
                 });
         return http.build();
     }
