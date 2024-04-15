07ec19534271c71a6e301db8833236e46b2b9bca
정우혁 <alalalalalalall367@gmail.com>
2024-04-15 15:58:12 +0900

    feat: GPT API를 이용하기 위한 dto 설계

---
+++ back-end/src/main/java/com/studymate/backend/global/gpt/dto/GPTRequest.java
@@ -0,0 +1,37 @@
+package com.studymate.backend.global.gpt.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class GPTRequest {
+    private String model;
+    private List<Message> messages;
+    private int temperature;
+    private int maxTokens;
+    private int topP;
+    private int frequencyPenalty;
+    private int presencePenalty;
+
+    public GPTRequest(String model,
+                      String prompt,
+                      int temperature,
+                      int maxTokens,
+                      int topP,
+                      int frequencyPenalty,
+                      int presencePenalty) {
+        this.model = model;
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user", prompt));
+        this.temperature = temperature;
+        this.maxTokens = maxTokens;
+        this.topP = topP;
+        this.frequencyPenalty = frequencyPenalty;
+        this.presencePenalty = presencePenalty;
+    }
+}
