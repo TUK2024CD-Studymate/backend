4536c56d169da71a773c4f822787c014affc14dd
정우혁 <alalalalalalall367@gmail.com>
2024-04-15 15:39:51 +0900

    feat: SecurityConfig 내부 허용 url 설정

--- back-end/src/main/java/com/studymate/backend/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/studymate/backend/config/security/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                         .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/api/chat/**").permitAll()
+                        .requestMatchers("/api/gpt/**").permitAll()
                         .requestMatchers("/api/logout").permitAll()
                         .requestMatchers("/subscribe/**").permitAll()
                         .anyRequest().authenticated()
