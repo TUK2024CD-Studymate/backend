832dd9e664fde041fdf772de1220d01d74288b9a
정우혁 <alalalalalalall367@gmail.com>
2024-04-15 15:57:27 +0900

    feat: QuestionController 내부 질문을 검사 및 만들어주는 api 설계

--- back-end/src/main/java/com/studymate/backend/question/controller/QuestionController.java
+++ back-end/src/main/java/com/studymate/backend/question/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package com.studymate.backend.question.controller;
 
+import com.studymate.backend.question.dto.QuestionAiRequest;
+import com.studymate.backend.question.dto.QuestionAiResponse;
 import com.studymate.backend.question.dto.QuestionCreateRequest;
 import com.studymate.backend.question.dto.QuestionResponse;
 import com.studymate.backend.question.service.QuestionService;
@@ -28,6 +30,13 @@ public class QuestionController {
         return ResponseEntity.ok().body(questionService.create(request));
     }
 
+    @PostMapping("/gpt/question")
+    @Operation(summary = "질문 검사 및 수정", description = "사용자가 작성한 질문을 맞춤법 검사 및 문장을 자연스럽게 만들어준다.")
+    @ApiResponses(value = @ApiResponse(responseCode = "201", description = "성공"))
+    public ResponseEntity<QuestionAiResponse> create(@RequestBody QuestionAiRequest request) {
+        return ResponseEntity.ok().body(questionService.createQuestionByAi(request));
+    }
+
     @GetMapping("/question/{questionId}")
     @Operation(summary = "질문 조회", description = "특정 질문을 불러온다")
     @ApiResponses(value = @ApiResponse(responseCode = "201", description = "성공"))
@@ -41,5 +50,4 @@ public class QuestionController {
     public ResponseEntity<List<QuestionResponse>> getQuestions() {
         return ResponseEntity.ok().body(questionService.getQuestions());
     }
-    //TODO 해결완료 유무로 질문 리스트 조회하는 api 설계?
 }
