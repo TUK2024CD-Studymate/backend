01a479dd206c543fc686fad50bd135e66455c493
jihye9871 <jihye9871@naver.com>
2024-04-27 16:49:58 +0900

    feat: ChatMessage 타입 없앰

--- back-end/src/main/java/com/studymate/backend/chat/domain/ChatMessage.java
+++ back-end/src/main/java/com/studymate/backend/chat/domain/ChatMessage.java
@@ -2,24 +2,30 @@ package com.studymate.backend.chat.domain;
 
 import java.time.LocalDateTime;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import com.studymate.backend.global.BaseTimeEntity;
+import com.studymate.backend.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
 
 @Getter
-@Setter
-@ToString
-@NoArgsConstructor
-public class ChatMessage {
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_message_id")
+    private Long id;
+    private String content;
+    private LocalDateTime sendDate;
 
-    // 메시지 타입 : 입장, 채팅
-    public enum MessageType {
-        ENTER, TALK
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
 
-    private MessageType type; // 메시지 타입
-    private String roomId; // 방번호
-    private String sender; // 메시지 보낸사람
-    private String message; // 메시지
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
 }
