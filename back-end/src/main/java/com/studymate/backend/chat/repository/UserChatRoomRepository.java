f0e32ffae693c844a3db9385f48a7b6bd7f7a741
jihye9871 <jihye9871@naver.com>
2024-05-04 17:17:14 +0900

    fix: 내 채팅방 조회시 반환값 수정

--- back-end/src/main/java/com/studymate/backend/chat/repository/UserChatRoomRepository.java
+++ back-end/src/main/java/com/studymate/backend/chat/repository/UserChatRoomRepository.java
@@ -4,10 +4,12 @@ import com.studymate.backend.chat.domain.UserChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserChatRoomRepository extends JpaRepository<UserChatRoom, Long> {
     boolean existsByMemberId(Long memberId);
-    Optional<UserChatRoom> findByMemberId(Long memberId);
+    List<UserChatRoom> findByMemberId(Long memberId);
+
 }
