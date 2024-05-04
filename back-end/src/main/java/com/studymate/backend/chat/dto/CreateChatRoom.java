7195a125ab9c6507f9978c320dc345e7b46f6aa4
jihye9871 <jihye9871@naver.com>
2024-05-04 17:16:37 +0900

    feat: 채팅방 생성시 필요한 CreateChatRoom dto 생성

---
+++ back-end/src/main/java/com/studymate/backend/chat/dto/CreateChatRoom.java
@@ -0,0 +1,12 @@
+package com.studymate.backend.chat.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateChatRoom {
+    private String name;
+}
