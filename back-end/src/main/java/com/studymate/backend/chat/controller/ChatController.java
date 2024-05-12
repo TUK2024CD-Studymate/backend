91b6eef86cb56a83160ea3ed44004ffe1820c839
jihye9871 <jihye9871@naver.com>
2024-05-12 23:46:59 +0900

    feat: 채팅방 입장시 읽음 처리 기능 구현

---
+++ back-end/src/main/java/com/studymate/backend/chat/controller/ChatController.java
@@ -0,0 +1,129 @@
+package com.studymate.backend.chat.controller;
+
+import com.studymate.backend.chat.domain.ChatRoom;
+import com.studymate.backend.chat.domain.UserChatRoom;
+import com.studymate.backend.chat.dto.ChatMessageRes;
+import com.studymate.backend.chat.dto.ChatRoomRes;
+import com.studymate.backend.chat.dto.CreateChatRoom;
+import com.studymate.backend.chat.service.ChatService;
+import com.studymate.backend.chat.service.MessageService;
+import com.studymate.backend.config.security.jwt.TokenProvider;
+import com.studymate.backend.member.MemberRepository;
+import com.studymate.backend.member.domain.Member;
+import com.studymate.backend.member.domain.UserDetail;
+import com.studymate.backend.member.service.CustomUserDetailsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Chat", description = "채팅 API")
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping("api/chat/rooms")
+public class ChatController {
+    private final ChatService chatService;
+    private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
+    private final MessageService messageService;
+
+    @Operation(summary = "chatroom create", description = "채팅방 생성")
+    @ApiResponses(value = @ApiResponse(responseCode = "201", description = "성공"))
+    @PostMapping
+    public ResponseEntity<?> createChatRoom(@RequestHeader("Authorization") String authHeader, @RequestBody CreateChatRoom createChatRoom) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: No token provided");
+        }
+        String token = authHeader.substring(7);
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        if (authentication == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: Invalid token");
+        }
+
+        User principal = (User) authentication.getPrincipal();
+        String email = principal.getUsername(); // JWT에서 추출된 사용자 이메일을 가져옵니다.
+        Member member = memberRepository.findByEmail(email);
+        if (member == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Member not found");
+        }
+
+        String chatRoomName = createChatRoom.getName();
+        if (chatRoomName == null || chatRoomName.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body("Chat room name is required");
+        }
+
+
+        ChatRoom chatRoom = chatService.createChatRoom(chatRoomName); // 서비스 계층에 이름을 전달
+        chatService.createUserChatRoom(member, chatRoom.getId());
+        ChatRoomRes chatRoomRes = ChatRoomRes.builder()
+                .chatRoomId(chatRoom.getId())
+                .chatRoomName(chatRoomName)// 채팅방 이름 포함
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(chatRoomRes);
+    }
+
+    @GetMapping("/api/chat/rooms/{chatRoomId}/enter")
+    public ResponseEntity<?> enterChatRoom(@PathVariable Long chatRoomId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = ((org.springframework.security.core.userdetails.User) authentication.getPrincipal()).getUsername();
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: Member not found");
+        }
+
+        // 채팅방 정보와 메시지 목록 조회
+        List<ChatMessageRes> messages = chatService.findChatMessage(chatRoomId, member.getId());
+        if (!messages.isEmpty()) {
+            ChatMessageRes lastMessage = messages.get(messages.size() - 1);
+            String lastMessageId = lastMessage.getMessageId().toString();  // 메시지 ID 게터 메소드 호출
+
+            // 사용자 읽음 마커 업데이트
+            messageService.updateUserReadPosition(chatRoomId.toString(), member.getId().toString(), lastMessageId);
+        }
+
+        return ResponseEntity.ok().body(messages);
+    }
+
+
+    @Transactional
+    @Operation(summary = "ChatRoomList read", description = "채팅방 목록 조회")
+    @ApiResponses(value = @ApiResponse(responseCode = "200", description = "성공"))
+    @GetMapping("/list")
+    public ResponseEntity<List<ChatRoomRes>> getChatRoomList(@RequestHeader("Authorization") String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token = authHeader.substring(7);
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        if (authentication == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        User principal = (User) authentication.getPrincipal();
+        String email = principal.getUsername(); // JWT에서 추출된 사용자 이메일을 가져옵니다.
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        List<ChatRoomRes> chatRoomList = chatService.findUserChatRoomByMemberId(member.getId());
+        return ResponseEntity.ok(chatRoomList);
+    }
+
+}
