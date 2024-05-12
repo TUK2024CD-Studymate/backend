bc057bd73e56634b080265ed251f813c37ddf750
jihye9871 <jihye9871@naver.com>
2024-05-12 23:45:21 +0900

    feat: 유저 추출 로직 수정

--- back-end/src/main/java/com/studymate/backend/chat/repository/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.studymate.backend.chat.repository;
-
-import com.studymate.backend.chat.domain.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-
-}
