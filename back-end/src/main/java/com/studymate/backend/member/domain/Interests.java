f852242e6edcb1190ef8538552a6799362a27a3b
정우혁 <alalalalalalall367@gmail.com>
2024-05-13 15:03:44 +0900

    refactor: 기존 회원 카테고리 세분화

--- back-end/src/main/java/com/studymate/backend/member/domain/Interests.java
+++ back-end/src/main/java/com/studymate/backend/member/domain/Interests.java
@@ -3,11 +3,11 @@ package com.studymate.backend.member.domain;
 
 public enum Interests {
 
-    PROGRAMMING("코딩"),
-    MATH("수학"),
-    ENGLISH("영어"),
-    SCIENCE("과학"),
-    KOREAN("국어");
+    WEBAPP("웹/앱"),
+    SERVER("서버/네트워크"),
+    AI("AI/IOT"),
+    DATA("데이터개발"),
+    SECURITY("보안");
 
     private final String korean;
 
