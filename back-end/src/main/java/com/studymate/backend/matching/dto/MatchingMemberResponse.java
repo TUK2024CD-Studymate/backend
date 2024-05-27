d02aebd7b9b1e3af0e2c6e98f30624cbdfc889f1
정우혁 <alalalalalalall367@gmail.com>
2024-05-27 23:37:37 +0900

    feat: AI매칭 퍼센트를 반환하기 위한 ResponseDTO 설계

---
+++ back-end/src/main/java/com/studymate/backend/matching/dto/MatchingMemberResponse.java
@@ -0,0 +1,36 @@
+package com.studymate.backend.matching.dto;
+
+import com.studymate.backend.member.domain.Interests;
+import com.studymate.backend.member.domain.Part;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatchingMemberResponse {
+    private Long id;
+    private String name;
+    private String nickname;
+    private Part part;
+    private String email;
+    private String tel;
+    private String expertiseField;
+    private Interests interests;
+    private String imageUrl;
+    private String blogUrl;
+    private String publicRelations;
+    private String job;
+    private int heart;
+    private BigDecimal starAverage;
+    private int solved;
+    private int matchingCount;
+    private int reviewCount;
+    private boolean isLogin;
+    private float matchingPercent;
+}
