0340ff074d845a7afc81c92182d136c8564d7e4f
정우혁 <alalalalalalall367@gmail.com>
2024-05-30 22:43:28 +0900

    refactor: ReviewMapper 내부 리뷰를 작성한 회원마다 프로필사진을 검증하는 로직 구현

--- back-end/src/main/java/com/studymate/backend/review/ReviewMapper.java
+++ back-end/src/main/java/com/studymate/backend/review/ReviewMapper.java
@@ -1,16 +1,23 @@
 package com.studymate.backend.review;
 
+import com.studymate.backend.file.ProfileImgRepository;
+import com.studymate.backend.file.domain.ProfileImg;
 import com.studymate.backend.member.domain.Member;
 import com.studymate.backend.review.domain.Review;
 import com.studymate.backend.review.dto.ReviewCreateRequest;
 import com.studymate.backend.review.dto.ReviewResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+
 @Component
+@RequiredArgsConstructor
 public class ReviewMapper {
+    private final ProfileImgRepository profileImgRepository;
 
     public Review toEntity(ReviewCreateRequest request, Member member) {
-
         return Review.builder()
                 .star(request.getStar())
                 .title(request.getTitle())
@@ -20,12 +27,20 @@ public class ReviewMapper {
     }
 
     public ReviewResponse toResponse(Review review) {
+        String imageUrl = "프로필 사진이 없습니다.";
+        Member member = review.getMember();
+        if (profileImgRepository.findByMember(member).isPresent()) {
+            Optional<ProfileImg> profileImg = profileImgRepository.findByMember(member);
+            imageUrl = profileImg.get().getUrl();
+        }
+
         return ReviewResponse.builder()
                 .isSolved(review.getIsSolved())
                 .reviewId(review.getId())
                 .writer(review.getWriter())
                 .mentor(review.getMentor())
                 .content(review.getContent())
+                .imageUrl(imageUrl)
                 .title(review.getTitle())
                 .heart(review.getHeart())
                 .star(review.getStar())
