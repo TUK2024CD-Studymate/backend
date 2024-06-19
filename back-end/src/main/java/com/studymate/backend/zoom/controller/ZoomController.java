b83894534384ebeabb3805b786f90434691b12d3
정우혁 <alalalalalalall367@gmail.com>
2024-06-19 20:06:34 +0900

    hotfix: 줌 리다리엑트 URL 변경

--- back-end/src/main/java/com/studymate/backend/zoom/controller/ZoomController.java
+++ back-end/src/main/java/com/studymate/backend/zoom/controller/ZoomController.java
@@ -52,7 +52,7 @@ public class ZoomController {
 
         FormBody formBody = new FormBody.Builder()
                 .add("code", code)
-                .add("redirect_uri", "http://3.36.177.42/api/meeting/zoomApi")
+                .add("redirect_uri", "http://34.64.61.117/api/meeting/zoomApi")
                 .add("grant_type", "authorization_code")
                 .add("code_verifier", DecEncUtil.encode(code))
                 .build();
