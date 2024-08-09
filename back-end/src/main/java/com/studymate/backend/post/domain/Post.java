30743f889401c2941a68e059726bf6fcee072589
정우혁 <alalalalalalall367@gmail.com>
2024-08-09 14:52:57 +0900

    feat: Post엔티티 내부 Fetch타입 EAGER로 변경

--- back-end/src/main/java/com/studymate/backend/post/domain/Post.java
+++ back-end/src/main/java/com/studymate/backend/post/domain/Post.java
@@ -22,7 +22,7 @@ public class Post extends BaseTimeEntity {
     @Column(name = "post_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="user_id",updatable = false)
     private Member member;
 
